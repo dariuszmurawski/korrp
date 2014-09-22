4ed38a0022241e24280bb597a1b35d71ad8aafb3
Darek <dariusz.murawski@pd.mofnet.gov.pl>
2014-09-22 14:01:48 +0200

    start pracy nad korrp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Korrp::Application.config.secret_key_base = '69ed9debb5097a4827f484288cf1ebec74f29df8a8d7f9ac2ffb362b0cda6ccff28d30a03d5758a82620521750f45baf0bac6a82b55a352966164b1a07b3bb1c'
